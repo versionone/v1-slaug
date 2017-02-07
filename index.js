635dc85d40148556f2aa2baea60da3de2e022500
rajiv <rajiv.delwadia@versionone.com>
2017-02-07 15:56:18 -0500

    simple log and response

--- index.js
+++ index.js
@@ -1,41 +1,26 @@
 const log = require('./lib/log')
-const util = require('util')
-var cool = require('cool-ascii-faces')
-var express = require('express');
-var app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
 
-app.set('port', (process.env.PORT || 61525));
+const parseBody = [
+	bodyParser.json({ strict: false }),
+	bodyParser.urlencoded({ extended: false }),
+	bodyParser.text({ type: '*/*' }),
+]
 
-app.use(express.static(__dirname + '/public'));
+const logRequest = (req, res, next) => {
+	log('request', req.method, req.originalUrl, req.get('Content-Type'), JSON.stringify(req.body))
+	next()
+}
 
-app.all('*', (req, res, next) => {
-	const reqObject = util.format(req).replace('<', '&lt;')
-	const resObject = util.format(res).replace('<', '&lt;')
-	const message = `<h1>Request</h1><pre>${reqObject}</pre><h1>Response</h1><pre>${resObject}</pre>`;
-	log(message)
-	res.send(message)
-})
+const dummyResponse = (req, res) => { res.send("Hello") }
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+const app = express();
+app.use(parseBody, logRequest, dummyResponse)
 
-app.get('/home', function(request, response) {
-  response.render('pages/index');
-});
+function start(app, port) {
+	app.set('port', port);
+	app.listen(port, log.defer('listening', port))
+}
 
-app.get('/cool', (req, res) => {
-	res.send(cool())
-})
-
-app.get('/times', (req, res) => {
-    var result = ''
-    var times = process.env.TIMES || 5
-    for (i=0; i < times; i++)
-      result += i + ' ';
-  res.send(result)
-})
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+start(app, process.env.PORT || 61525)
