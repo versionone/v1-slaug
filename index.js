e28db1e07bbca8322c9ffa8e8f88afc8a15ff5f5
rajiv <rajiv.delwadia@versionone.com>
2017-02-07 17:05:33 -0500

    timing

--- index.js
+++ index.js
@@ -2,21 +2,34 @@ const log = require('./lib/log')
 const express = require('express')
 const bodyParser = require('body-parser')
 
+function processingTimer(req, res, next) {
+	const start = Date.now()
+	res.on('finish', () => log('response', Date.now() - start))
+	next()
+}
+
 const parseBody = [
 	bodyParser.json({ strict: false }),
 	bodyParser.urlencoded({ extended: false }),
 	bodyParser.text({ type: '*/*' }),
 ]
 
-const logRequest = (req, res, next) => {
+function logRequest(req, res, next) {
 	log('request', req.method, req.originalUrl, req.get('Content-Type'), JSON.stringify(req.body))
 	next()
 }
 
-const dummyResponse = (req, res) => { res.send("Hello") }
+function generateResponse(req, res) {
+	 res.send("Hello")
+}
 
 const app = express();
-app.use(parseBody, logRequest, dummyResponse)
+app.use(
+	processingTimer,
+	parseBody,
+	logRequest,
+	generateResponse
+)
 
 function start(app, port) {
 	app.set('port', port);
