fd409c0bcea36465e5b665cc0dbdf4e464ecff49
rajiv <rajiv.delwadia@versionone.com>
2017-02-09 10:56:18 -0500

    fetch asset data from V1

--- v1-slaug.js
+++ v1-slaug.js
@@ -6,6 +6,8 @@ const request = require('request-promise')
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const truthy = value => !!value
+
 const _recentlyExpanded = new Set()
 const isRecentlyExpanded = (key) => _recentlyExpanded.has(key.toLowerCase())
 const rememberExpanded = (key) => {
@@ -30,7 +32,6 @@ function logRequest(req, res, next) {
 	log('request', req.method, req.originalUrl, req.get('Content-Type'), JSON.stringify(req.body))
 	next()
 }
-
 
 const assettypeMap = {
 	at: ['Test', 'BaseAsset'],
@@ -58,40 +59,130 @@ const assettypeMap = {
 	ts: ['TestSet', 'BaseAsset'],
 }
 
-function generateResponse(req, res) {
+function respond(req, res) {
 	const requestText = req.body && req.body.text
 	if (!requestText) return res.end()
 
-	const responses = []
-	const rx = /\b([A-Z]+)-\d+\b/ig
+	const matches = findMatches(requestText, /\b([A-Z]+)-\d+\b/ig, match => ({
+		number: match[0].toUpperCase(),
+		assettype: assettypeMap[ match[1].toLowerCase() ][0],
+		order: match.index,
+	}))
+	if (!matches.length) return res.end()
+
+	const promisedMessages = matches
+		.map(expandAssetReference)
+
+	Promise.all(promisedMessages)
+		.then(messages => {
+			const responseText = messages.filter(truthy).join('\n')
+			res
+				.type('application/json')
+				.send({ text: responseText })
+		})
+		.catch(errorResponse(res))
+}
+
+const errorResponse = (function() {
+	if (process.env.NODE_ENV === 'production') {
+		return res => err => {
+			log('ERROR', err.message)
+			res.end()
+		}
+	}
+	else {
+		return res => err => {
+			log('ERROR', err.message)
+			res.status(500).send(err)
+		}
+	}
+})()
+
+function findMatches(text, rx, map) {
+	const matches = []
 	let match
-	while ((match = rx.exec(requestText))) {
-		const id = match[0]
-		if (isRecentlyExpanded(id)) continue
+	while ((match = rx.exec(text)))
+		matches.push(map(match))
+	return matches
+}
 
-		const type = match[1].toLowerCase()
-		const assettype = assettypeMap[type]
-		if (!assettype) continue
+const instance = process.env.V1_INSTANCE
+if (!instance)
+	throw new Error("V1_INSTANCE is not defined")
+const urlBase = instance + '/rest-1.v1/Data/'
 
-		responses.push(formatResponse(id, assettype))
-		rememberExpanded(id)
+const accessToken = process.env.V1_ACCESSTOKEN
+if (!accessToken)
+	throw new Error("V1_ACCESSTOKEN is not defined")
+const Authorization = 'Bearer ' + accessToken
+
+const v1request = request.defaults({
+	jar: true,
+	method: 'GET',
+	qs: {
+		deleted: 'true',
+		sel: 'Name,AssetState,Number',
+	},
+	headers: {
+		'User-Agent': 'v1-slaug',
+		Accept: 'application/json',
+		Authorization,
+	},
+})
+
+function expandAssetReference(ref) {
+	if (isRecentlyExpanded(ref.number)) return null
+	if (!ref.assettype) return null
+
+	const url = urlBase + ref.assettype
+	const where = `Number='${ref.number}'`
+
+	return v1request({ url, qs:{ where } })
+		.then(JSON.parse)
+		.then(response => {
+			if (!response || !response.Assets || !response.Assets.length) return null
+			const asset = response.Assets[0]
+			const attributes = asset.Attributes
+			return {
+				id: asset.id,
+				number: attributes.Number.value,
+				name: attributes.Name.value,
+				state: attributes.AssetState.value,
+			}
+		})
+		.then(rememberExpansion)
+		.then(formatResponse)
+}
+
+function rememberExpansion(asset) {
+	rememberExpanded(asset.id)
+	rememberExpanded(asset.number)
+	return asset
+}
+
+const formatResponse = (function() {
+	const encodings = {
+		'&': '&amp;',
+		'<': '&lt;',
+		'>': '&gt;',
 	}
 
-	if (!responses.length) return res.end()
-	const responseText = responses.join('\n')
-	return res.send(JSON.stringify({ text: responseText }))
-}
+	const encoding = char => encodings[char]
+	const encode = text => text.replace(/[&<>]/g, encoding)
 
-function formatResponse(id, assettype) {
-	 return `*<https://www7.v1host.com/V1Production/assetdetail.v1?Number=${id}|${id}>* <https://www7.v1host.com/V1Production/rest-1.v1/Data/${assettype[0]}?deleted=true&amp;accept=application/json&amp;sel=Name,AssetState,Number&amp;where=Number=%27${id}%27|(rest-1)>`
-}
+	return (asset) => {
+		if (!asset) return null
+		const encodedName = encode(asset.name)
+		return `*${asset.number}* <https://www7.v1host.com/V1Production/assetdetail.v1?Number=${asset.number}|${encodedName}>`
+	}
+})()
 
 const app = express();
 app.use(
 	processingTimer,
 	parseBody,
 	logRequest,
-	generateResponse
+	respond
 )
 
 function start(app, port) {
